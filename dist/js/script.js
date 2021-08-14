3ee58be2baa2f61a12064d1afe0ee006186c8cab
Ivan <fenomen_ny@mail.ru>
2021-08-14 22:56:49 +0300

    flex

---
+++ dist/js/script.js
@@ -0,0 +1,16 @@
+const btnAdd = document.querySelector('.btn'),
+	inputName = document.querySelector('#name'),
+	inputDescr = document.querySelector('#description'),
+	inputLink = document.querySelector('#link'),
+	inputPrice = document.querySelector('#price');
+
+	// console.log(inputPrice);
+
+// btnAdd.addEventListener('click', (e) => {
+// 	e.preventDefault();
+
+// });
+// document.querySelector('form').submit(function (e) {
+// 	e.preventDefault();
+
+// });
