a23fcf37db7b9e1d0d3ef1a689b5ed96a5d87632
Ivan <fenomen_ny@mail.ru>
2021-08-18 03:10:19 +0300

    card del & img

--- src/js/script.js
+++ src/js/script.js
@@ -1,49 +1,41 @@
 window.addEventListener('DOMContentLoaded', () => {
-const hamburger = document.querySelector('.hamburger'),
-  form = document.querySelector('.form'),
-  formClose = document.querySelector('.form__close'),
-  bodyLock = document.querySelector('body'),
-  mainCard = document.querySelectorAll('.main__card');
+  const hamburger = document.querySelector('.hamburger'),
+    form = document.querySelector('.form'),
+    formClose = document.querySelector('.form__close'),
+    bodyLock = document.querySelector('body'),
+    mainCard = document.querySelectorAll('.main__card'),
+    img = document.querySelectorAll('.card__del');
 
-
-// hamburger
-hamburger.addEventListener('click', () => {
-  form.classList.add('active');
-  bodyLock.classList.add('lock');
-});
-
-formClose.addEventListener('click', () => {
-  form.classList.remove('active');
-  bodyLock.classList.remove('lock');
-});
-
-// animation
-mainCard.forEach(item => {
-  item.addEventListener('mouseenter', () => {
-    item.style.cssText = `animation: card 1.5s 1 ease-in-out; cursor: pointer`;
+  // animation & img
+  mainCard.forEach((item, i) => {
+    item.addEventListener('mouseenter', () => {
+      item.style.cssText = `animation: card 1.5s 1 ease-in-out; cursor: pointer;`;
+      img[i].classList.add('img_del');
+    });
   });
-});
 
-mainCard.forEach(item => {
-  item.addEventListener('mouseleave', () => {
-    item.style.cssText = `animation: none; border: none`;
+  mainCard.forEach((item, i) => {
+    item.addEventListener('mouseleave', () => {
+      item.style.cssText = `animation: none; border: none`;
+      img[i].classList.remove('img_del');
+    });
   });
-});
 
-// const btnAdd = document.querySelector('.btn'),
-// 	inputName = document.querySelector('#name'),
-// 	inputDescr = document.querySelector('#description'),
-// 	inputLink = document.querySelector('#link'),
-// 	inputPrice = document.querySelector('#price');
+  // delCard
+  img.forEach((item, i) => {
+    item.addEventListener('click', () => {
+      mainCard[i].remove();
+    });
+  });
 
-// console.log(inputPrice);
+  // hamburger
+  hamburger.addEventListener('click', () => {
+    form.classList.add('active');
+    bodyLock.classList.add('lock');
+  });
 
-// btnAdd.addEventListener('click', (e) => {
-// 	e.preventDefault();
-
-// });
-// document.querySelector('form').submit(function (e) {
-// 	e.preventDefault();
-
-// });
+  formClose.addEventListener('click', () => {
+    form.classList.remove('active');
+    bodyLock.classList.remove('lock');
+  });
 });
